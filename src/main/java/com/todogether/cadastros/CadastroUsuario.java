1813b7c9b7336fc503a654baae613bccd82379d9
Felipe França <faf2@cin.ufpe.br>
2022-08-14 21:28:42 -0300

    varios TODO

--- src/main/java/com/todogether/cadastros/CadastroUsuario.java
+++ src/main/java/com/todogether/cadastros/CadastroUsuario.java
@@ -1,5 +1,22 @@
 package com.todogether.cadastros;
 
+import com.todogether.entity.Usuario;
+
 public class CadastroUsuario {
+
+	public boolean adicionarUsuario(Usuario usuario) {
+		// TODO
+		return false;
+	}
+
+	public boolean existe(Long id) {
+		// TODO
+		return false;
+	}
+
+	public Usuario consultarUsuario(String email) {
+		// TODO
+		return null;
+	}
 
 }
